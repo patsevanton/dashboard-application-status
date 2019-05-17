fb88c3fde5049941d59063d160ea49a568850f9b
Anton Patsev <patsev.anton@gmail.com>
2019-05-17 16:46:00 +0600

    Add gorutine

--- main.go
+++ main.go
@@ -1,23 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
+  "fmt"
+  "html/template"
+  "log"
+  "net/http"
+  "os/exec"
+  "time"
 )
 
 func main() {
-    url := "http://10.2.64.110:8080/monitoring/selftest"
-    resp, err := http.Get(url)
-    if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
-    html, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Printf("%s\n",html)
+  log.SetFlags(log.Lshortfile)
+  appVersion := []uint8{}
 
+  go func() {
+    for {
+      cmd := "/usr/bin/curl -I 'https://google.com/' 2>/dev/null | grep -i '^date:' | sed 's/^[Dd]ate: //g'"
+      appVersion, err := exec.Command("bash","-c",cmd).Output()
+      if err != nil {
+          log.Fatal(err)
+      }
+      fmt.Printf("%s", appVersion)
+      time.Sleep(5 * time.Second)
+    }
+  }
+
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+   tmpl, err := template.ParseFiles("index.html")
+   if err != nil {
+     http.Error(w, err.Error(), http.StatusInternalServerError)
+   }
+
+   data := struct {
+     AppVersion string
+   }{
+     AppVersion: string(appVersion[:]),
+   }
+
+   err = tmpl.Execute(w, data)
+   if err != nil {
+     http.Error(w, err.Error(), http.StatusInternalServerError)
+   }
+  })
+
+  fmt.Println("http://127.0.0.1:3000")
+  log.Fatalln(http.ListenAndServe("127.0.0.1:3000", nil))
 }
+
 
